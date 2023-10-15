4192d9f620d6a2c5bef735cb9146c8e3e71b5ed6
Alanderson Araujo <alandersoncaraujo@gmail.com>
2023-10-15 18:39:44 -0300

    feat: Transaction Model

---
+++ src/main/java/br/com/alphablack/stone/model/transaction/Transaction.java
@@ -0,0 +1,24 @@
+package br.com.alphablack.stone.model.transaction;
+
+import br.com.alphablack.stone.model.client.Client;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Client client;
+    private Integer value;
+    private Date date;
+
+}
